e9bfb56429d979bbfa58b95525a8b1c177d284f4
GabrielHenriquedev <ghss14@discente.ifpe.edghss14@discente.ifpe.ed>
2021-07-06 20:00:29 -0300

    criaçao do cadastro de cliente

--- src/main/java/com/ifpe/veterinario/controllers/AtendenteController.java
+++ src/main/java/com/ifpe/veterinario/controllers/AtendenteController.java
@@ -1,7 +1,29 @@
 package com.ifpe.veterinario.controllers;
 
+import com.ifpe.veterinario.models.Cliente;
+import com.ifpe.veterinario.repositiry.Clienterepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AtendenteController {
+    @Autowired
+    private Clienterepository er;
+
+    @GetMapping("/cadastro-cliente")
+    public ModelAndView casdastroCliente(){
+        ModelAndView cd = new ModelAndView( "atendimentos/CDclients");
+        cd.addObject("cliente", new Cliente());
+        return cd;
+    }
+    @PostMapping("/cadastro-cliente")
+    public ModelAndView cadastrocliente(Cliente cliente){
+        ModelAndView cd = new ModelAndView("atendimentos/CDclients");
+        er.save(cliente);
+        return cd;
+
+    }
 }
--- src/main/java/com/ifpe/veterinario/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ifpe.veterinario.repository;
-
-import com.ifpe.veterinario.models.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-}
